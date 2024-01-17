de20ab7d67cdc2f2d9598cf7685a1228b4307f31
gsurendhar <135189441+gsurendhar@users.noreply.github.com>
2024-01-17 14:49:20 +0530

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,28 @@
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+
+
+
+wget https://releases.hashicorp.com/terraform/1.6.6/terraform_1.6.6_linux_386.zip
+unzip terraform_1.6.6_linux_386.zip
+mv terraform /user/local/bin or /user/bin
+
+# main.tf
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "rama"
+}
