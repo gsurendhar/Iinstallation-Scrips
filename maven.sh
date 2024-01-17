97e64c05a1e563687923b62a6fae2016c52b5733
gsurendhar <135189441+gsurendhar@users.noreply.github.com>
2024-01-17 15:33:02 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -24,3 +24,26 @@ export PATH=$M2_HOME/bin/:$PATH
 # source /etc/profile.d/maven.sh
 # mvn -version
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Main phases
+clean   —----> delete target directory
+validate —---> validate, if the project is correct
+compile  —---> compile source code, classes stored in target/classes
+test  ---—---> run tests
+package —----> take the compiled code and package it in its distributable format, e.g. JAR, WAR
+verify —-----> run any checks to verify the package is valid and meets quality criteria
+install —----> install the package into the local repository
+deploy —-----> copies the final package to the remote repository
