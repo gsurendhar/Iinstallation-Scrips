5187cd10cb9d853d2ab040041c36280c747785e0
gsurendhar <135189441+gsurendhar@users.noreply.github.com>
2024-01-17 13:35:46 +0530

    Create nexus.sh

---
+++ nexus.sh
@@ -0,0 +1,23 @@
+# Java must required
+java -version
+# java not installed use this
+# sudo apt-get install openjdk-8-jdk
+java -version
+sudo apt install net-tools
+cd /opt/
+wget https://download.sonatype.com/nexus/3/nexus-3.64.0-04-unix.tar.gz
+ls -l
+tar -zvxf nexus-3.64.0-04-unix.tar.gz 
+ls -la
+adduser nexus
+ls -la
+chown -R nexus:nexus nexus-3.64.0-04
+chown -R nexus:nexus sonatype-work
+ls -la
+vi nexus-3.64.0-04/bin/nexus.rc             # (run-as-user="nexus") 
+# login to nexus user and try
+su - nexus
+/opt/nexus-3.64.0-04/bin/nexus start
+ps -ef | grep nexus
+tail -f /opt/sonatype-work/nexus3/log/nexus.log
+# port is 8081
